02a68c55eeaff0565ee7058d527d9cb9839b4c2c
rlynn1 <rlynn1@mail.csuchico.edu>
2017-04-25 13:42:09 -0700

    Random string choice
    Randomly chooses from an array of strings without choosing the same string twice in a row.

--- gallery.js
+++ gallery.js
@@ -1,6 +1,13 @@
-
+var chico = ["One Mile", "Upper Park", "North Rim", "Neighborhood", "Sheridan Ave"];
+var current;
 
 function shuffleGallery()
 {
-
+  do
+  {
+    var input = Math.floor(Math.random() * chico.length);
+  }
+  while(current == input);
+  console.log(chico[input]);
+  current = input;
 }
